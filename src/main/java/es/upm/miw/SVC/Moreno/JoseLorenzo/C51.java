252880c045b7f760a34cf19e264fe496648ec3bd
Jose Lorenzo <jolorenzom@gmail.com>
2018-09-16 20:12:19 +0200

    Creación e implementación de la clase C51 con sus métodos m1 y m2

---
+++ src/main/java/es/upm/miw/SVC/Moreno/JoseLorenzo/C51.java
@@ -0,0 +1,14 @@
+package es.upm.miw.SVC.Moreno.JoseLorenzo;
+
+public class C51 {
+
+    public C51(){}
+
+    public String m1() {
+        return "m1";
+    }
+
+    public String m2() {
+        return "m2";
+    }
+}
